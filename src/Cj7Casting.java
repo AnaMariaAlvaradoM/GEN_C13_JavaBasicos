05ccb9eb3181cab81149c6a93a6804d5d4e7a011
 <anaa_alvarad@hotmail.com>
2026-08-05 10:47:45 -0500

    prueba

--- src/Cj7Casting.java
+++ src/Cj7Casting.java
@@ -9,7 +9,6 @@ public class Cj7Casting {
         int entero = (int) precio2;
         System.out.println(entero);
 
-
         int grande = 2147483647;
         int resultado = grande + 1;
         System.out.println(resultado);
